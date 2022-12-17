6d3d1efbcc1fb7aae3c4bbaa46dcea4c3fc8efd3
olegpl1993 <olegpl1993@gmail.com>
2022-12-17 23:40:57 +0200

    feat: create img type module

--- src/modules/header/header.ts
+++ src/modules/header/header.ts
@@ -1,6 +1,7 @@
 import './header.scss';
 import { createElement } from '../createElement'
-import { route } from '../..';
+import { route } from '../..'
+import cart from '../../images/cart.png'
 
 export function header(headerBox: HTMLElement) {
   const header = createElement(headerBox, 'div', 'header');
@@ -9,6 +10,8 @@ export function header(headerBox: HTMLElement) {
   const linkMain = createElement(menu, 'a', 'link linkMain', 'Main Page');
   const linkCart = createElement(menu, 'a', 'link linkCart', 'Cart Page');
   const linkProduct = createElement(menu, 'a', 'link linkProduct', 'Product Page');
+  const cartImg = createElement(header, 'img', 'cartImg');
+  (cartImg as HTMLImageElement).src = cart;
 
   (linkMain as HTMLAnchorElement).href = '#';
   (linkCart as HTMLAnchorElement).href = '#cart';
