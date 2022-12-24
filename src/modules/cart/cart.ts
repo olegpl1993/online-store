0f2a2c3aa5b5effbac4959e872bd59b1e5657582
olegpl1993 <olegpl1993@gmail.com>
2022-12-24 22:25:21 +0200

    feat: cart layout

--- src/modules/cart/cart.ts
+++ src/modules/cart/cart.ts
@@ -22,19 +22,38 @@ export function cart(contentBox: HTMLElement) {
   const optionPageNumber = createElement(productsListOption, 'div', 'optionPageNumber', '1');
   const optionPageNextBtn = createElement(productsListOption, 'button', 'optionPageNextBtn', '>');
 
+  // список товаров -------------------------------------------------------------------------
   const cartStateObj = cartState.map((product) => product.id) // { ID товара : количество товара }
     .reduce((acc: any, el: number) => { acc[el] = (acc[el] || 0) + 1; return acc }, {}); // TODO (типизировать ANY)
   console.log(cartState)
   console.log(cartStateObj)
+  console.log(products)
 
   const productsList = createElement(productsBox, 'div', 'productsList');
   let i = 1; // начальная строка товара
   for (const id in cartStateObj) {
     const productRow = createElement(productsList, 'div', 'productRow');
-    const numberRow = createElement(productRow, 'div', 'numberRow', `${i++}`);
-    const productTitle = createElement(productRow, 'div', 'productTitle', `${products[+id - 1].title}`);
-  }
 
+    const numberRow = createElement(productRow, 'div', 'numberRow', `${i++}`);
+    const productImg = createElement(productRow, 'img', 'productImg');
+    (productImg as HTMLImageElement).src = products[+id - 1].thumbnail;
+
+    const descriptionColum = createElement(productRow, 'div', 'descriptionColum');
+    createElement(descriptionColum, 'div', 'productTitle', `${products[+id - 1].title}`);
+    createElement(descriptionColum, 'div', 'descriptionText', `Category: ${products[+id - 1].category}`);
+    createElement(descriptionColum, 'div', 'descriptionText', `Brand: ${products[+id - 1].brand}`);
+    createElement(descriptionColum, 'div', 'descriptionText', `Discount Percentage: ${products[+id - 1].discountPercentage}`);
+    createElement(descriptionColum, 'div', 'descriptionText', `Rating: ${products[+id - 1].rating}`);
+    createElement(descriptionColum, 'div', 'descriptionText', `${products[+id - 1].description}`);
+
+    const numberColum = createElement(productRow, 'div', 'numberColum'); // колонка регулировки количества товара
+    const numberProductStock = createElement(numberColum, 'div', 'numberProductStock', `Stock: ${products[+id - 1].stock}`);
+    const numberAddAwayRow = createElement(numberColum, 'div', 'numberAddAwayRow');
+    const numberAddBtn = createElement(numberAddAwayRow, 'button', 'numberBtn', '+');
+    const numberOfProducts = createElement(numberAddAwayRow, 'div', 'numberOfProducts', `${cartStateObj[id]}`);
+    const numberAwayBtn = createElement(numberAddAwayRow, 'button', 'numberBtn', '-');
+    const numberProductSumm = createElement(numberColum, 'div', 'numberProductSumm', `$${products[+id - 1].price * cartStateObj[id]}`);
+  }
 
 
   // блок подсчета суммы ------------------------------------------------------------------
