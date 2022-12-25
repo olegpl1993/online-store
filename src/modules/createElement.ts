09661d7a8b88691d7249d814796b790516ed41de
VladBoisa <vladboisa@gmail.com>
2022-12-25 21:06:05 +0200

    refactor: contant can be and element too

--- src/modules/createElement.ts
+++ src/modules/createElement.ts
@@ -1,4 +1,4 @@
-export function createElement(parent: HTMLElement, tag: string, className?: string, content?: string | number) {
+export function createElement(parent: HTMLElement|Element, tag: string, className?: string, content?: string | number | HTMLElement) {
   const element = document.createElement(tag);
   if (className) element.className = className;
   if (content) element.textContent = String(content);
