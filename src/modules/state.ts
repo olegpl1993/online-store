0b38877f2875413d5ff0979f056e1f824b5bcfe9
VladBoisa <vladboisa@gmail.com>
2022-12-26 14:47:47 +0200

    fix: make state a copy arr products not a link

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -3,6 +3,6 @@ import { Product } from '../types/types';
 
 export const products = data.products; // получаем массив продуктов до сортировки
 
-export const state = products; // отсортированный массив (текущее состояние)
+export const state = [...products]; // отсортированный массив (текущее состояние)
 
 export const cartState: Product[] = []; // массив продуктов в корзине
