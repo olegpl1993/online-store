33df2c82de8510369d290ff886c6e8d3ef3f8012
olegpl1993 <olegpl1993@gmail.com>
2022-12-27 03:26:31 +0200

    feat: query sorting in url

--- src/modules/functions.ts
+++ src/modules/functions.ts
@@ -1,21 +1,16 @@
 import { Product } from "../types/types";
 import { state } from "./state";
 
-//Сортировка по признаку и значению поля
-//TODO типизация any
-/*
-export function sortProductsByFieldAndType (sortType:string,sortFieldName:string){ //функция сортировки массива объектов по признаку(убывание,возрастание) и значению
-  if (sortType === 'Descending') {
-    return (a:any, b:any) => a[sortFieldName.toLowerCase()] > b[sortFieldName.toLowerCase()] ? -1 : 1;
-  } else if(sortType === 'Ascending') {
-    return (a:any, b:any) => a[sortFieldName.toLowerCase()] > b[sortFieldName.toLowerCase()] ? 1 : -1;
-  }
-}
-*/
-
 export function sortState(sortType: string) {
   if (sortType === 'priceup') state.sort((a: Product, b: Product) => a.price - b.price);
   if (sortType === 'pricedown') state.sort((a: Product, b: Product) => b.price - a.price);
   if (sortType === 'ratingup') state.sort((a: Product, b: Product) => a.rating - b.rating);
   if (sortType === 'ratingdown') state.sort((a: Product, b: Product) => b.rating - a.rating);
+}
+
+export function parseSearch(hash: string) {
+  const queryString = hash.split('?')[1]; // отделяет поиск от хеша
+  const queryArr = queryString ? queryString.split('=') : []; // разделение в массив
+  const queryObj = { [queryArr[0]]: queryArr[1] }; // обьект из масива
+  return queryObj;
 }
